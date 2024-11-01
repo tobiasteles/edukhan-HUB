bc7e27b05d4be40a9287af873a1b38cbba65d095
Tobias Teles <tobias.teles@gmail.com>
2024-11-01 11:18:57 -0300

    Update folder

--- public/index.php
+++ public/index.php
@@ -1,2 +1,8 @@
 <?php
+print_r($_SERVER['REQUEST_URI']);
 
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+} else {
+    echo "<p>Welcome to PHP</p>";
+}
